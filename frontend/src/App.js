f466f92b81958e1df42fb9123be314a02272450c
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2023-05-16 23:04:35 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
+import Activate from "./pages/home/activate";
+import Reset from "./pages/reset";
+import CreatePostPopup from "./components/createPostPopup";
+import { useSelector } from "react-redux";
+function App() {
+  const { user } = useSelector((state) => ({ ...state }));
+  return (
+    <div>
+      <CreatePostPopup user={user} />
+      <Routes>
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+          <Route path="/activate/:token" element={<Activate />} exact />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} exact />
+        </Route>
+        <Route path="/reset" element={<Reset />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
